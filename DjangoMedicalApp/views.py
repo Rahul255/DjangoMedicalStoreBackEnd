d5bff67e75aba099f7ebfc78d24951f3b7f7ebac
Rahul255 <rahulmohan255@gmail.com>
2021-02-09 18:53:04 +0530

    file added

--- DjangoMedicalApp/views.py
+++ DjangoMedicalApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.generics import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.response import Response
 from DjangoMedicalApp.models import Company
@@ -6,12 +7,35 @@ from DjangoMedicalApp.serializers import ComapnySerializer
 
 
 # Create your views here.
+# company view 
 class ComapnyViewSet(viewsets.ViewSet):
-    
+    # list,create,update company data
     def list(self,request):
         company = Company.objects.all()
         serializer = ComapnySerializer(company,many=True,context={"request":request})
         response_dict= {"error":False,"message":"All Company List Data","data":serializer.data}
         return Response(response_dict)
+    def create(self,request):
+        try:
+            serializer = ComapnySerializer(data = request.data,context={"request":request})
+            serializer.is_valid()
+            serializer.save()
+            response_dict= {"error":False,"message":"Company data save successfully"}
+        except:
+            response_dict= {"error":True,"message":"Error during saving company data"}
+        return Response(response_dict)
+
+    def update(self,request,pk=None):
+        try:
+            queryset =  Company.objects.all()
+            company = get_object_or_404(queryset,pk=pk)
+            serializer = ComapnySerializer(company,data = request.data,context={"request":request})
+            serializer.is_valid()
+            serializer.save()
+            response_dict= {"error":True,"message":"Successfully updated company data"}
+        except:
+            response_dict= {"error":True,"message":"Error during updating company data"}
+
     
 company_list = ComapnyViewSet.as_view({"get":"list"})
+company_create = ComapnyViewSet.as_view({"post":"create"})
